b2545f27ab953306d75cf9f37b4e70a325a6cc1e
Jason Miller <jason@developit.ca>
2016-01-11 20:54:48 -0500

    use cors proxy

--- src/peach.js
+++ src/peach.js
@@ -1,7 +1,9 @@
 import store from './store';
 import peachClient from 'peach-client';
 
-let peach = peachClient({ store, init:false });;
+let url = 'https://cors-anywhere.herokuapp.com/v1.peachapi.com:443';
+
+let peach = peachClient({ url, store, init:false });;
 window.peach = peach;
 export default peach;
 
